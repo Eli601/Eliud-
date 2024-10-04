06721a66f845408920311bcbf2ee09b6493c2dca
Eli601 <eliudkairu7@gmail.com>
2024-10-04 09:22:06 +0300

    Add files via upload

---
+++ bank (1).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+    int age;
+    float income;
+    
+    printf("Enter your age:");
+    scanf("%d,"&age:);
+
+    printf("Enter your annual income:")
+    scanf("%f",&income);
+
+    if(age>=21 && income>=2100) {
+        printf("congratulations you qualify")
+    } else {
+        ("unfortunately we are unable")
+    }
+
+    return 0;
+    
+    }
